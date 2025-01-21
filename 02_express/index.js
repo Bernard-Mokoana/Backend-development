e08f089d3d7f8e7d7f0563d2d601597d753b041e
Bernard <bernarddira@gmail.com>
2025-01-21 11:34:51 +0200

    "Advance logging with morgan and winston"

--- 02_express/index.js
+++ 02_express/index.js
@@ -1,15 +1,36 @@
 import express from 'express';
+import logger from './logger.js';
+import morgan from 'morgan';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+app.use(express.json());
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+    morgan(morganFormat, {
+      stream: {
+        write: (message) => {
+          const logObject = {
+            method: message.split(" ")[0],
+            url: message.split(" ")[1],
+            status: message.split(" ")[2],
+            responseTime: message.split(" ")[3],
+          };
+          logger.info(JSON.stringify(logObject));
+        },
+      },
+    })
+  );
 
 let gamesData = [];
 let nextId = 1;
 
-app.use(express.json());
+
 
 app.post('/games', (req, res) => {
-
+    logger.info('A post request was made')
     const {name, price} = req.body
     const newGame = {id: nextId++, name, price}
     gamesData.push(newGame)
